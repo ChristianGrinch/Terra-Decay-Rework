fcb641e93afd9a552bf931148f51b9089e681781
ChristianGrinch <christiangingrich12707@Gmail.com>
2025-03-06 18:58:09 -0800

    began writing uimanager
    - started to write the uimanager script
    - added random other files apparently idk where they came from

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public enum Menu
+    {
+        Start,
+        Settings,
+    }
+    public List<Menu> navigationHistory;
+
+    public void ToggleMenu(Menu menuToToggle)
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
