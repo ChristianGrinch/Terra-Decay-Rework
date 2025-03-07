7c5e29910e67887175856be1dcbc86b4d8f564c5
ChristianGrinch <christiangingrich12707@Gmail.com>
2025-03-07 11:10:00 -0800

    more navigation history logic implimentation
    - added initalize menus method in order to initalize the menu gameobjects
    there was supposed to be a sttings menu created but the program crashed due to the while loop and i didnt save

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
+
 public enum Menu
 {
     Start,
@@ -8,16 +10,26 @@ public enum Menu
 }
 public class UIManager : MonoBehaviour
 {
-    public Interface[] navigationHistory;
-    public GameObject[] menusGameObjects;
+    public List<Interface> navigationHistory;
+    public GameObject[] menuGameObjects;
+    public Canvas parentCanvas;
     
     void Start()
-    {
-        navigationHistory.Append(new Interface { menu = Menu.Start,  gameObject = menusGameObjects[0] });
+    { 
+        InitializeMenus();
+        navigationHistory.Append(new Interface { menu = Menu.Start,  gameObject = menuGameObjects[0] });
     }
-    void Update()
+
+    public void InitializeMenus()
     {
-        
+        while (menuGameObjects.Length < parentCanvas.transform.childCount -1) menuGameObjects.Append(null);
+        for (var i = 0; i < parentCanvas.transform.childCount -1; i++)
+        {
+            if (parentCanvas.transform.GetChild(i).gameObject.GetComponent<Canvas>())
+            {
+                menuGameObjects[i] = parentCanvas.transform.GetChild(i).gameObject;
+            }
+        }
     }
     public void ToggleMenuStatus(Menu menuToToggle)
     {
@@ -25,12 +37,28 @@ public class UIManager : MonoBehaviour
     }
     public bool GetMenuStatus(Menu menu)
     {
-        return navigationHistory.Contains(new Interface {menu = menu} );
+        return navigationHistory.Contains(new Interface { menu = menu } );
     }
 
     public void SetMenuStatus(Menu menu, bool status)
     {
         
+    }
+
+    public void OpenMenu(Interface @interface)
+    {
+        if (navigationHistory.Contains(@interface)) return;
+        
+        navigationHistory.Append(@interface);
+        @interface.gameObject.SetActive(true);
+    }
+
+    public void CloseMenu(Interface @interface)
+    {
+        if(!navigationHistory.Contains(@interface)) return;
+        
+        navigationHistory.Remove(@interface);
+        @interface.gameObject.SetActive(false);
     }
 }
 
