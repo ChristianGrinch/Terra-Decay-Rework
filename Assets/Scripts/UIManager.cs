a1db21614971c0b77f37495e9bfa2ea677a15bd6
ChristianGrinch <christiangingrich12707@Gmail.com>
2025-03-06 23:02:45 -0800

    Update UIManager.cs
    - attempted to add interface logic. most likely will have to redo

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,25 +1,41 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-
+public enum Menu
+{
+    Start,
+    Settings,
+}
 public class UIManager : MonoBehaviour
 {
-    public enum Menu
-    {
-        Start,
-        Settings,
-    }
-    public List<Menu> navigationHistory;
-
-    public void ToggleMenu(Menu menuToToggle)
-    {
-        
-    }
+    public Interface[] navigationHistory;
+    public GameObject[] menusGameObjects;
+    
     void Start()
     {
-        
+        navigationHistory.Append(new Interface { menu = Menu.Start,  gameObject = menusGameObjects[0] });
     }
     void Update()
     {
         
     }
+    public void ToggleMenuStatus(Menu menuToToggle)
+    {
+        
+    }
+    public bool GetMenuStatus(Menu menu)
+    {
+        return navigationHistory.Contains(new Interface {menu = menu} );
+    }
+
+    public void SetMenuStatus(Menu menu, bool status)
+    {
+        
+    }
+}
+
+public class Interface
+{
+    public Menu menu;
+    public GameObject gameObject;
 }
